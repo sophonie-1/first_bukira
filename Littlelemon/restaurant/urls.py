2fb4c3af5fd78ea60a09d7e1c16d92437a85644d
bukira <bukirasophonie347@gmail.com>
2025-01-07 16:35:58 +0300

    thid commit to my projecct

--- Littlelemon/restaurant/urls.py
+++ Littlelemon/restaurant/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-from .views import index
+from django.urls import path,include
+from .views import MenuSerializerView,MenuSingleView,BookingViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'tables',BookingViewSet)
 
 
 
 urlpatterns = [
-    path('',index,name='index')
+    path('menu/',MenuSerializerView.as_view(),name='menu'),
+    path('menu/<int:pk>/',MenuSingleView.as_view(),name='menu-detail'),
+    path('restaurant/booking/',include(router.urls))
 ]
